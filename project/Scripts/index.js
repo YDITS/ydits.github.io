08bf0410a701c3c2ba92c2704f571538a4dcfb3c
よね/Yone <66835584+yone1130@users.noreply.github.com>
2023-01-27 22:55:13 +0900

    Update site

---
+++ project/Scripts/index.js
@@ -0,0 +1,21 @@
+// 
+// index.js | Project | YDITS Official site 
+//
+// (c) 2023 よね/Yone
+// 
+
+$(document).on('DOMContentLoaded', form());
+
+function form(){
+
+    let formPath = "https://docs.google.com/forms/d/e/1FAIpQLSce_20PsxNGSIf893ybw00MGUI1X20WrprNBkAhC8L3lHW7eA/viewform";
+
+    $(document).on('click', '#sendBtn', function(){
+        if (document.getElementById('agreeChk').checked){
+            console.log('clicked')
+            window.open(formPath, '_blank');
+        } else {
+            $('#error').addClass('show');
+        }
+    });
+}
