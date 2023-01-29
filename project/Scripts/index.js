12bb7cfda71c50e6a401042f59a8f5c681dd5a0d
よね/Yone <66835584+yone1130@users.noreply.github.com>
2023-01-29 15:52:59 +0900

    Update site

--- project/Scripts/index.js
+++ project/Scripts/index.js
@@ -1,7 +1,7 @@
 // 
 // index.js | Project | YDITS Official site 
 //
-// (c) 2023 よね/Yone
+// (c) 2022-2023 よね/Yone
 // 
 
 $(document).on('DOMContentLoaded', form());
@@ -11,7 +11,7 @@ function form(){
     let formPath = "https://docs.google.com/forms/d/e/1FAIpQLSce_20PsxNGSIf893ybw00MGUI1X20WrprNBkAhC8L3lHW7eA/viewform";
 
     $(document).on('click', '#sendBtn', function(){
-        if (document.getElementById('agreeChk').checked){
+        if (document.getElementById('agreeChk').checked && document.getElementById('ageChk').checked) {
             console.log('clicked')
             window.open(formPath, '_blank');
         } else {
