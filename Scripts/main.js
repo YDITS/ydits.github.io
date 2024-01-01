7d953f923564a732db7966525ec7b352bf190e23
yone1130 <admin@yoneyo.com>
2024-01-02 01:37:36 +0900

    Emergency Information Close

--- Scripts/main.js
+++ Scripts/main.js
@@ -60,18 +60,4 @@ $(() => {
             langChange(lang);
         });
     });
-
-
-    // 2024-01-01 Tsunami Information
-    $("main").prepend(`
-        <a id="emergency" href="https://webapp.ydits.net/">
-            <p>津波警報、津波注意報が発表されています（20時30分）</p>
-        </a>
-    `);
-    $("#emergency").css({
-        "display": "block",
-        "background-color": "#f04040",
-        "padding": "1em",
-        "color": "#ffffffff"
-    });
 });
