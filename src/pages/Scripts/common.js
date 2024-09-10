aaca4a5904e468c179009beb47b9d8f4edc5bea1
よね/Yone <git@yoneyo.com>
2024-09-10 12:12:28 +0900

    Change: Update `src/pages/Scripts/common.js`

--- src/pages/Scripts/common.js
+++ src/pages/Scripts/common.js
@@ -10,78 +10,57 @@
  */
 
 (() => {
+    const pageLangages = [
+        "ja-jp",
+        "en-us",
+        "zh-cn",
+        "zh-tw",
+        "ko-kr"
+    ]
 
-const pageLangages = [
-    "ja-jp",
-    "en-us",
-    "zh-cn",
-    "zh-tw",
-    "ko-kr"
-]
+    const langChange = (lang) => {
+        let path = location.pathname;
+        let pathArray = path.split("/");
+        let langValue = lang;
 
-const headerInnerHtml = `
-<div class="content">
-    <a id="headerLogo" href="./">
-        <img src="https://cdn.ydits.net/images/ydits_logos/ydits_logo_white_transparent.png" alt="YDITS icon">
-    </a>
-
-    <div id="headerMenuButton">
-        <span class="material-symbols-outlined open">menu</span>
-        <span class="material-symbols-outlined close">close</span>
-    </div>
-
-    <nav id="headerMenu">
-        <ul>
-            <li><a href="./">ホーム</a></li>
-            <li><a href="https://www.yoneyo.com/#contact">お問い合わせ</a></li>
-        </ul>
-    </nav>
-</div>
-`;
-
-const langChange = (lang) => {
-    let path = location.pathname;
-    let pathArray = path.split("/");
-    let langValue = lang;
-
-    if (pageLangages.includes(pathArray[1])) {
-        if (langValue == "ja-jp") {
-            location.pathname = path.slice(6);
+        if (pageLangages.includes(pathArray[1])) {
+            if (langValue == "ja-jp") {
+                location.pathname = path.slice(6);
+            } else {
+                location.pathname = langValue + path.slice(6);
+            }
         } else {
-            location.pathname = langValue + path.slice(6);
-        }
-    } else {
-        if (langValue == "ja-jp") {
-            location.pathname = path;
-        } else {
-            location.pathname = langValue + path;
+            if (langValue == "ja-jp") {
+                location.pathname = path;
+            } else {
+                location.pathname = langValue + path;
+            }
         }
     }
-}
 
-$(() => {
-    let isActive_headerLink = false;
+    $(() => {
+        let isActive_headerLink = false;
 
-    document.getElementsByTagName("header")[0].innerHTML = headerInnerHtml;
+        $("header").load("/elements/header.html");
 
-    $(document).on('click', '#headerMenuButton', () => {
-        $('#headerMenuButton').toggleClass('opened');
-        $('#headerMenu').toggleClass('active');
-    });
+        $(document).on('click', '#headerMenuButton', () => {
+            $('#headerMenuButton').toggleClass('opened');
+            $('#headerMenu').toggleClass('active');
+        });
 
-    $("footer").load("./elements/footer.html");
+        $("footer").load("/elements/footer.html");
 
-    // Language select
-    const langSelectLabelQuery = '#langSelect .label';
-    $(document).on('click', langSelectLabelQuery, () => {
-        $('#langSelect').toggleClass('active');
+        // Language select
+        const langSelectLabelQuery = '#langSelect .label';
+        $(document).on('click', langSelectLabelQuery, () => {
+            $('#langSelect').toggleClass('active');
 
-    });
-    pageLangages.forEach(lang => {
-        $(document).on('click', `#langSelect .${lang}`, () => {
-            langChange(lang);
+        });
+        pageLangages.forEach(lang => {
+            $(document).on('click', `#langSelect .${lang}`, () => {
+                langChange(lang);
+            });
         });
     });
-});
 
 })();
